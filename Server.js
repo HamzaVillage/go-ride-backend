cbbbe7d3acd0e68890044fc0578a2f7fd3de8fbf
Hamza village <hamzavillage@Hamzas-MacBook-Pro.local>
2026-02-23 07:12:14 +0500

    port update

--- Server.js
+++ Server.js
@@ -32,7 +32,7 @@ app.use("/address", addressRoutes);
 
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
